4a3cd3163a4d9ef8446cdc225fb8537098f783c2
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-12-11 15:08:04 +0100

    commit update

--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -37,3 +37,62 @@ public class Truck implements Runnable {
         }
     }
 }
+//Truck1.java
+//        package ait.elevator.task;
+//
+//        import ait.elevator.model.Elevator;
+//
+//public class Truck1 implements Runnable {
+//    static Object monitor = new Object();
+//    int nRaces;
+//    int capacity;
+//    Elevator[] elevators;
+//
+//    public Truck1(int nRaces, int capacity, Elevator... elevators) {
+//        this.nRaces = nRaces;
+//        this.capacity = capacity;
+//        this.elevators = elevators;
+//    }
+//
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < nRaces; i++) {
+//            synchronized (monitor) {
+//                elevators[0].add(capacity / elevators.length);
+//                elevators[1].add(capacity / elevators.length);
+//            }
+//        }
+//    }
+//
+//}
+
+//Truck2.java
+//        package ait.elevator.task;
+//
+//        import ait.elevator.model.Elevator;
+//
+//public class Truck2 implements Runnable {
+//    private static Object monitor1 = new Object();
+//    private static Object monitor2 = new Object();
+//    int nRaces;
+//    int capacity;
+//    Elevator[] elevators;
+//
+//    public Truck2(int nRaces, int capacity, Elevator... elevators) {
+//        this.nRaces = nRaces;
+//        this.capacity = capacity;
+//        this.elevators = elevators;
+//    }
+//
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < nRaces; i++) {
+//            synchronized (monitor1) {
+//                elevators[0].add(capacity / elevators.length);
+//            }
+//            synchronized (monitor2) {
+//                elevators[1].add(capacity / elevators.length);
+//            }
+//        }
+//    }
+//}
